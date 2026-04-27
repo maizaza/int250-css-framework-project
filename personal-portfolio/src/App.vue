40c1c618d1904045c9f2981b99d5b69f2f5f2836
Siwach Saeoung <maizaza@users.noreply.github.com>
2026-04-27 23:40:02 +0700

    add personal-portfolio vite template

---
+++ personal-portfolio/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// Start coding here!
+</script>
+
+<template>
+  <div class="max-w-4xl mx-auto p-8 text-center">
+    <h1 class="text-4xl font-bold text-blue-600 mb-4">
+      New Project: personal-portfolio
+    </h1>
+    <p class="text-gray-600">
+      Edit <code class="bg-gray-100 p-1 rounded">src/App.vue</code> to get started.
+    </p>
+  </div>
+</template>
